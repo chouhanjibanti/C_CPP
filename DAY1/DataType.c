6f8f7c583a8d1f022480341fb8af085f8c47bc39
chouhanjibanti <chouhanbanti224@gmail.com>
2025-11-18 22:47:46 +0530

    first commit

---
+++ DAY1/DataType.c
@@ -0,0 +1,52 @@
+// normal program
+// #include<stdio.h>
+
+// int main(){
+//     int age = 22;
+//     float price = 100.99;
+//     printf("%d",age);
+//     printf("%f",price);
+//    return 0;
+// }
+
+// 1. WAP add two number.
+// #include<stdio.h>
+// void main(){
+//     int a = 10 , b = 20 , c;
+//     c = a+b;
+//     printf("Sum = %d",c);
+// }
+// ===========================
+
+// 2, WAP to print the sum of two number taking the input from the user.
+
+// #include<stdio.h>
+// void main(){
+//     int a , b,c;
+//     printf("Enter the value of a,b\n");
+//     scanf("%d%d" , &a,&b);
+//     c = a+b;// 100 // 200
+//     printf("Sum =%d\n",c);
+// }
+
+
+// 3. WAP to find the simple interest.
+#include<stdio.h>
+void Main(){
+    float p , r , t , si;
+    printf("Enter the value of p, r , t\n");
+    scanf("%f%f%f",&p,&r,&t);
+    si = (p*r*t)/100;
+    printf("Simple interst = %f",si);
+}
+
+// 4. WAP to find the area of circle.
+// 5. WAP to swap two numbers using the third number.
+// 6. WAP to swap two numbers without using the third number.
+// 7. WAP to find the average of the students. 
+// 8. WAP to find the area of square.
+// 9. WAP to find the ascii value of the character.
+
+
+
+
